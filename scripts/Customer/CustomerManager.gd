6f682d3c835d14e5000ce23b4b38f88e79d9637f
RafliNST <rafli.ran3103@gmail.com>
2026-04-01 20:43:20 +0700

    make customer order feature

--- scripts/Customer/CustomerManager.gd
+++ scripts/Customer/CustomerManager.gd
@@ -2,6 +2,18 @@ extends Node
 class_name CustomerManager
 
 @export var customer_scene: PackedScene
-@export var left_spawn_point: Node2D
-@export var right_spawn_point: Node2D
+@export var spawn_points: Array[Node2D]
 @export var table_points: Array[Node2D]
+
+@export var menu_selection: Array[Menu]
+
+@onready var new_customer_timer := $SpawnNewCustomer
+
+func _ready() -> void:
+	new_customer_timer.start()
+
+func _on_spawn_new_customer_timeout() -> void:
+	var customer = customer_scene.instantiate()
+	spawn_points.pick_random().add_child(customer)
+	customer.initialize(table_points.pick_random(), 
+		menu_selection.pick_random())
---
+++ scripts/Customer/customer.gd
@@ -0,0 +1,30 @@
+extends Node2D
+class_name Customer
+
+@export var mov_speed := 10.0
+
+@onready var order_icon := $OrderNotifier/OrderIcon
+
+var is_reach := false
+
+var origin_pos := Vector2.ZERO
+var seat_pos: Node2D
+var menu: Menu
+var is_reached_seat := false
+
+func _process(delta: float) -> void:
+	move_to_(seat_pos.global_position, delta)
+
+func initialize(seat: Node2D, menu: Menu) -> void:
+	seat_pos = seat
+	order_icon.texture = menu.icon
+	
+func move_to_(target: Vector2, delta: float) -> void:
+	if is_reach:
+		return
+		
+	global_position = global_position.move_toward(seat_pos.global_position, 
+		mov_speed * delta)
+	
+	if global_position.distance_to(target) < .1:
+		is_reach = true
