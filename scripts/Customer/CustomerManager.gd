0d65eba2b8f0377ca0ddee32085b61df086e6267
RafliNST <rafli.ran3103@gmail.com>
2026-04-01 21:50:55 +0700

    check table availability before spawn

--- scripts/Customer/CustomerManager.gd
+++ scripts/Customer/CustomerManager.gd
@@ -1,19 +1,48 @@
 extends Node
 class_name CustomerManager
 
+@export var elapse_time: float
+
 @export var customer_scene: PackedScene
 @export var spawn_points: Array[Node2D]
-@export var table_points: Array[Node2D]
 
+enum TABLE_AVAILABILITY { NO, YES }
+@export var table_points: Array[Node2D]
 @export var menu_selection: Array[Menu]
+
+var tables_dict := { }
 
 @onready var new_customer_timer := $SpawnNewCustomer
 
 func _ready() -> void:
+	for table in table_points:
+		tables_dict[table] = TABLE_AVAILABILITY.YES
+		
+	new_customer_timer.wait_time = elapse_time
 	new_customer_timer.start()
 
+func get_available_tables() -> Array[Node2D]:
+	var tables: Array[Node2D]
+	for table in table_points:
+		if tables_dict[table] == TABLE_AVAILABILITY.YES:
+			tables.append(table)
+		
+	return tables
+	
+func set_table(point: Node2D, status: TABLE_AVAILABILITY) -> void:
+	if tables_dict.has(point):
+		tables_dict[point] = status
+	
 func _on_spawn_new_customer_timeout() -> void:
+	var table = get_available_tables()
+	var menu = menu_selection.pick_random()
+	
+	if table.size() <= 0:
+		return
+	
+	table = table.pick_random()
+	set_table(table, TABLE_AVAILABILITY.NO)
+	
 	var customer = customer_scene.instantiate()
 	spawn_points.pick_random().add_child(customer)
-	customer.initialize(table_points.pick_random(), 
-		menu_selection.pick_random())
+	customer.initialize(table,menu)
--- scripts/Customer/customer.gd
+++ scripts/Customer/customer.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name Customer
 
-@export var mov_speed := 10.0
+@export var mov_speed := 300.0
 
 @onready var order_icon := $OrderNotifier/OrderIcon
 
@@ -12,12 +12,16 @@ var seat_pos: Node2D
 var menu: Menu
 var is_reached_seat := false
 
+var is_delivered := false
+
 func _process(delta: float) -> void:
 	move_to_(seat_pos.global_position, delta)
 
 func initialize(seat: Node2D, menu: Menu) -> void:
 	seat_pos = seat
 	order_icon.texture = menu.icon
+	
+	order_icon.hide()
 	
 func move_to_(target: Vector2, delta: float) -> void:
 	if is_reach:
@@ -28,3 +32,4 @@ func move_to_(target: Vector2, delta: float) -> void:
 	
 	if global_position.distance_to(target) < .1:
 		is_reach = true
+		order_icon.show()
